d04f148cfb85553ac97572a568ad0c2d9e9c5ddb
Jalal Manafi <calalmnf@fmail.com>
2023-03-29 00:12:51 +0400

    :recycle: Font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ const PostView = (props: PostWithUser) => {
           <span>@{author.username}</span> ·
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
